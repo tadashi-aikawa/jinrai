7660936baaf44cdd03ae7a0418ce60c496c032b7
tadashi-aikawa <syou.maman@gmail.com>
2026-04-16 08:43:30 +0900

    chore: v0.10.0

--- Jinrai.spoon/init.lua
+++ Jinrai.spoon/init.lua
@@ -1,6 +1,6 @@
 local obj = {
 	name = "Jinrai",
-	version = "0.9.0",
+	version = "0.10.0",
 	author = "tadashi-aikawa",
 	license = "MIT - https://github.com/tadashi-aikawa/jinrai/blob/main/LICENSE",
 	homepage = "https://github.com/tadashi-aikawa/jinrai",
